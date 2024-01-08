c92bf9ac96c5b9db02995e4132083ce0b9651270
David Mulder <dmulder@samba.org>
2024-01-08 11:14:07 -0700

    Add the PRT request
    Signed-off-by: David Mulder <dmulder@samba.org>

--- src/auth.rs
+++ src/auth.rs
@@ -7,6 +7,19 @@ use tracing::error;
 use urlencoding::encode as url_encode;
 use uuid::Uuid;
 
+#[cfg(feature = "prt")]
+use compact_jwt::crypto::JwsTpmSigner;
+#[cfg(feature = "prt")]
+use compact_jwt::jws::JwsBuilder;
+#[cfg(feature = "prt")]
+use compact_jwt::traits::JwsMutSigner;
+#[cfg(feature = "prt")]
+use kanidm_hsm_crypto::{BoxedDynTpm, IdentityKey};
+#[cfg(feature = "prt")]
+use os_release::OsRelease;
+#[cfg(feature = "prt")]
+use serde::Serialize;
+
 pub const INVALID_CRED: u32 = 0xC3CE;
 pub const REQUIRES_MFA: u32 = 0xC39C;
 pub const INVALID_USER: u32 = 0xC372;
@@ -14,6 +27,9 @@ pub const NO_CONSENT: u32 = 0xFDE9;
 pub const NO_GROUP_CONSENT: u32 = 0xFDEA;
 pub const NO_SECRET: u32 = 0x6AD09A;
 pub const AUTH_PENDING: u32 = 0x11180;
+
+#[cfg(feature = "prt")]
+const BROKER_CLIENT_IDENT: &str = "38aa3b87-a06d-4817-b275-7a316988d93b";
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct ErrorResponse {
@@ -30,6 +46,10 @@ pub enum MsalError {
     AcquireTokenFailed(ErrorResponse),
     /// Failure encountered in the reqwest module
     RequestFailed,
+    /// The Authentication type is not supported
+    AuthTypeUnsupported,
+    /// Failure encountered interacting with the TPM
+    TPMFail(String),
 }
 
 /* RFC8628: 3.2. Device Authorization Response */
@@ -150,6 +170,95 @@ pub struct UserToken {
     pub id_token: IdToken,
     #[serde(deserialize_with = "decode_client_info", default)]
     pub client_info: ClientInfo,
+}
+
+#[cfg(feature = "prt")]
+pub enum Credentials {
+    UsernamePassword(String, String),
+    RefreshToken(String),
+    SAMLToken(String),
+}
+
+#[cfg(feature = "prt")]
+#[derive(Serialize, Clone, Default)]
+struct UsernamePasswordAuthenticationPayload {
+    client_id: String,
+    request_nonce: String,
+    scope: String,
+    win_ver: Option<String>,
+    grant_type: String,
+    username: String,
+    password: String,
+}
+
+#[cfg(feature = "prt")]
+impl UsernamePasswordAuthenticationPayload {
+    fn new(username: &str, password: &str, request_nonce: &str) -> Self {
+        let os_release = match OsRelease::new() {
+            Ok(os_release) => Some(format!(
+                "{} {}",
+                os_release.pretty_name, os_release.version_id
+            )),
+            Err(_) => None,
+        };
+        UsernamePasswordAuthenticationPayload {
+            client_id: BROKER_CLIENT_IDENT.to_string(),
+            request_nonce: request_nonce.to_string(),
+            scope: "openid aza ugs".to_string(),
+            win_ver: os_release,
+            grant_type: "password".to_string(),
+            username: username.to_string(),
+            password: password.to_string(),
+        }
+    }
+}
+
+#[cfg(feature = "prt")]
+#[derive(Serialize, Clone, Default)]
+struct RefreshTokenAuthenticationPayload {
+    client_id: String,
+    request_nonce: String,
+    scope: String,
+    win_ver: Option<String>,
+    grant_type: String,
+    refresh_token: String,
+}
+
+#[cfg(feature = "prt")]
+impl RefreshTokenAuthenticationPayload {
+    fn new(refresh_token: &str, request_nonce: &str) -> Self {
+        let os_release = match OsRelease::new() {
+            Ok(os_release) => Some(format!(
+                "{} {}",
+                os_release.pretty_name, os_release.version_id
+            )),
+            Err(_) => None,
+        };
+        RefreshTokenAuthenticationPayload {
+            client_id: BROKER_CLIENT_IDENT.to_string(),
+            request_nonce: request_nonce.to_string(),
+            scope: "openid aza ugs".to_string(),
+            win_ver: os_release,
+            grant_type: "refresh_token".to_string(),
+            refresh_token: refresh_token.to_string(),
+        }
+    }
+}
+
+#[cfg(feature = "prt")]
+#[derive(Debug, Deserialize)]
+struct Nonce {
+    #[serde(rename = "Nonce")]
+    nonce: String,
+}
+
+#[cfg(feature = "prt")]
+#[derive(Debug, Clone, Deserialize)]
+pub struct PrimaryRefreshToken {
+    pub refresh_token: String,
+    pub refresh_token_expires_in: u64,
+    pub session_key_jwe: String,
+    pub id_token: String,
 }
 
 pub struct PublicClientApplication {
@@ -362,6 +471,123 @@ impl PublicClientApplication {
             })?;
 
             Ok(token)
+        } else {
+            let json_resp: ErrorResponse = resp.json().await.map_err(|e| {
+                error!("{}", e);
+                MsalError::InvalidJson
+            })?;
+            Err(MsalError::AcquireTokenFailed(json_resp))
+        }
+    }
+
+    #[cfg(feature = "prt")]
+    async fn request_nonce(&self) -> Result<String, MsalError> {
+        let resp = self
+            .client
+            .post(format!(
+                "https://{}/common/oauth2/token",
+                self.authority_host
+            ))
+            .body("grant_type=srv_challenge")
+            .send()
+            .await
+            .map_err(|e| {
+                error!("{}", e);
+                MsalError::RequestFailed
+            })?;
+        if resp.status().is_success() {
+            let json_resp: Nonce = resp.json().await.map_err(|e| {
+                error!("{}", e);
+                MsalError::InvalidJson
+            })?;
+            Ok(json_resp.nonce)
+        } else {
+            let json_resp: ErrorResponse = resp.json().await.map_err(|e| {
+                error!("{}", e);
+                MsalError::InvalidJson
+            })?;
+            Err(MsalError::AcquireTokenFailed(json_resp))
+        }
+    }
+
+    #[cfg(feature = "prt")]
+    pub async fn request_user_prt(
+        &self,
+        credentials: Credentials,
+        tpm: &mut BoxedDynTpm,
+        id_key: &IdentityKey,
+    ) -> Result<PrimaryRefreshToken, MsalError> {
+        let nonce = self.request_nonce().await?;
+
+        // [MS-OAPXBC] 3.2.5.1.2 POST (Request for Primary Refresh Token)
+        let jwt = JwsBuilder::from(match credentials {
+            Credentials::UsernamePassword(uname, pass) => serde_json::to_vec(
+                &UsernamePasswordAuthenticationPayload::new(&uname, &pass, &nonce),
+            )
+            .map_err(|e| {
+                error!("Failed serializing UsernamePassword JWT: {}", e);
+                MsalError::InvalidJson
+            })?,
+            Credentials::RefreshToken(refresh_token) => serde_json::to_vec(
+                &RefreshTokenAuthenticationPayload::new(&refresh_token, &nonce),
+            )
+            .map_err(|e| {
+                error!("Failed serializing RefreshToken JWT: {}", e);
+                MsalError::InvalidJson
+            })?,
+            _ => return Err(MsalError::AuthTypeUnsupported),
+        })
+        .set_typ(Some("JWT"))
+        .build();
+
+        let mut jws_tpm_signer = match JwsTpmSigner::new(tpm, id_key) {
+            Ok(jws_tpm_signer) => jws_tpm_signer,
+            Err(e) => {
+                return Err(MsalError::TPMFail(format!(
+                    "Failed loading tpm signer: {}",
+                    e
+                )))
+            }
+        };
+
+        let signed_jwt = match jws_tpm_signer.sign(&jwt) {
+            Ok(signed_jwt) => signed_jwt,
+            Err(e) => return Err(MsalError::TPMFail(format!("Failed signing jwk: {}", e))),
+        };
+
+        let params = [
+            ("windows_api_version", "2.0"),
+            ("grant_type", "urn:ietf:params:oauth:grant-type:jwt-bearer"),
+            ("request", &format!("{}", signed_jwt)),
+            ("client_info", "1"),
+            ("tgt", "true"),
+        ];
+        let payload = params
+            .iter()
+            .map(|(k, v)| format!("{}={}", k, v))
+            .collect::<Vec<String>>()
+            .join("&");
+
+        let resp = self
+            .client
+            .post(format!(
+                "https://{}/{}/oauth2/token",
+                self.authority_host, self.tenant_id
+            ))
+            .header(header::CONTENT_TYPE, "application/x-www-form-urlencoded")
+            .body(payload)
+            .send()
+            .await
+            .map_err(|e| {
+                error!("{}", e);
+                MsalError::RequestFailed
+            })?;
+        if resp.status().is_success() {
+            let json_resp: PrimaryRefreshToken = resp.json().await.map_err(|e| {
+                error!("{}", e);
+                MsalError::InvalidJson
+            })?;
+            Ok(json_resp)
         } else {
             let json_resp: ErrorResponse = resp.json().await.map_err(|e| {
                 error!("{}", e);
